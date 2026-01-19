0806f914a8e0c4ee7e40af34d2059de9a44285cb
Ana Regina Múzquiz <anareginarodriguez2006@gmail.com>
2026-01-19 13:54:39 -0600

    Update Sorts.h

--- Sorts.h
+++ Sorts.h
@@ -1,1 +1,17 @@
+#ifndef SORTS_H
+#define SORTS_H
+
+#include <vector>
+#include "RegistroBitacora.h"
+
+class Sorts {
+public:
+    // Ordenamiento burbuja
+    // Complejidad: O(n^2) tiempo, O(1) espacio
+    static void bubbleSort(std::vector<RegistroBitacora>& datos,
+                           long long& comparaciones,
+                           long long& swaps);
+};
+
+#endif
 
