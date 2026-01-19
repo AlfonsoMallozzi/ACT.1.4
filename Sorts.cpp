87370083c77d91acc032c9658dba2541d2bbf1d6
Ana Regina Múzquiz <anareginarodriguez2006@gmail.com>
2026-01-19 13:55:03 -0600

    Update Sorts.cpp

--- Sorts.cpp
+++ Sorts.cpp
@@ -1,1 +1,24 @@
+#include "Sorts.h"
+
+void Sorts::bubbleSort(std::vector<RegistroBitacora>& datos,
+                       long long& comparaciones,
+                       long long& swaps) {
+    int n = datos.size();
+
+    comparaciones = 0;
+    swaps = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            comparaciones++;
+
+            if (datos[j + 1] < datos[j]) {
+                RegistroBitacora temp = datos[j];
+                datos[j] = datos[j + 1];
+                datos[j + 1] = temp;
+                swaps++;
+            }
+        }
+    }
+}
 
